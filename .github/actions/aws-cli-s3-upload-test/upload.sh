6b97e4add9b38a5d3ab9c7965a183c9f267155bc
Kyle Huntsman <3432646+kylehuntsman@users.noreply.github.com>
2023-07-28 17:50:51 -0700

    feat: add aws cli s3 upload test workflow to pull requests
    Adds a GitHub action and workflow that creates a file, uploads the file via the AWS CLI S3 API, downloads the file, and then compares the contents of both files to test that the Motion storage backend is working properly.

---
+++ .github/actions/aws-cli-s3-upload-test/upload.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Create a test file to upload
+echo fish > upload.txt
+
+# Export the AWS credentials and region
+export AWS_ACCESS_KEY_ID=accessKey1
+export AWS_SECRET_ACCESS_KEY=verySecretKey1
+export AWS_DEFAULT_REGION=location-motion-v1
+
+# Create a test bucket, retrying if it fails.
+# We do this to ensure we're not failing too early on a connection error.
+RETRY_NUM=3
+RETRY_EVERY=15
+NUM=$RETRY_NUM
+until aws --endpoint-url http://127.0.0.1:8000 s3 mb s3://test-bucket
+do
+  1>&2 echo Retrying $NUM more times
+  sleep $RETRY_EVERY
+  ((NUM--))
+
+  if [ $NUM -eq 0 ]
+  then
+    1>&2 echo Bucket was not created after $RETRY_NUM tries
+    exit 1
+  fi
+done 
+
+aws --endpoint-url http://127.0.0.1:8000 s3 cp upload.txt s3://test-bucket
+aws --endpoint-url http://127.0.0.1:8000 s3 cp s3://test-bucket/upload.txt download.txt
+cmp upload.txt download.txt
+
+# Compare the downloaded file with the original and echo if successful
+if cmp upload.txt download.txt
+then
+  1>&2 echo "Uploaded and downloaded files are the same"
+else
+  1>&2 echo "Uploaded and downloaded files are different"
+  exit 1
+fi
